0fb883109155e9cdf93dba7316f8568979305eb1
Martynas-J <Aurimhas@gmail.com>
2023-09-11 21:05:23 +0300

    game fix route fech 1,23

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   async headers() {
     return [
       {
-        // matching all API routes
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
