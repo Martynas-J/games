8f973cd9dbc282b46b538370ec91720abd031c46
Martynas-J <Aurimhas@gmail.com>
2023-09-11 16:15:40 +0300

    game fix route fech 1,19

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://martynas-game.vercel.app/api/:path*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
