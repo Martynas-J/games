c271d483e1874da3de49e4e90f65020e52576265
Martynas-J <Aurimhas@gmail.com>
2023-09-13 11:17:40 +0300

    game fix route fech 1,39

--- src/app/api/getResults/route.js
+++ src/app/api/getResults/route.js
@@ -2,8 +2,6 @@ import connect from "@/app/utils/db";
 import playerModel from "@/models/Player";
 import { NextResponse } from "next/server";
 
-export const revalidate = true;
-
 export const GET = async (req) => {
   if (req.method === "GET") {
     try {
