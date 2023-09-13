c4592ae9d993cf06089a54cfcdead58b1afd20a5
Martynas-J <Aurimhas@gmail.com>
2023-09-13 14:22:59 +0300

    game page style

--- src/app/config/config.js
+++ src/app/config/config.js
@@ -1,2 +1,2 @@
-// export const API_URL = "https://martynas-game.vercel.app"
-export const API_URL = "http://localhost:3000"
+export const API_URL = "https://martynas-game.vercel.app"
+// export const API_URL = "http://localhost:3000"
