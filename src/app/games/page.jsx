a11d97d0435a496f757bbdd3d5ed54ad9c439eaa
Martynas-J <Aurimhas@gmail.com>
2023-09-13 11:59:49 +0300

    game page style

--- src/app/games/page.jsx
+++ src/app/games/page.jsx
@@ -8,12 +8,11 @@ import { saveResultData } from "@/components/saveResultData";
 import { shuffleQuestions } from "@/components/shuffleQuestions";
 
 const Quiz = () => {
-
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
   const { data, mutate } = useSWR(`${API_URL}/api/getResults`, fetcher);
 
   const [questionsList, setQuestionsList] = useState(
-    shuffleQuestions(questions).slice(0, 10) 
+    shuffleQuestions(questions).slice(0, 10)
   );
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [score, setScore] = useState(0);
@@ -32,7 +31,7 @@ const Quiz = () => {
     if (nextQuestionIndex < questionsList.length) {
       setCurrentQuestionIndex(nextQuestionIndex);
     } else {
-      setQuestionsList(shuffleQuestions(questions).slice(0, 10)); 
+      setQuestionsList(shuffleQuestions(questions).slice(0, 10));
       setCurrentQuestionIndex(0);
       setShowScore(true);
       setGameFinished(true);
@@ -41,7 +40,7 @@ const Quiz = () => {
 
   const saveResult = async () => {
     try {
-      const response = await saveResultData(playerName, score)
+      const response = await saveResultData(playerName, score);
       if (response.ok) {
         setResultSaved(true);
         mutate();
@@ -55,15 +54,16 @@ const Quiz = () => {
 
   useEffect(() => {
     if (showScore) {
-      setQuestionsList(shuffleQuestions(questions).slice(0, 10)); 
+      setQuestionsList(shuffleQuestions(questions).slice(0, 10));
       setPlayerScore(score);
     }
   }, [showScore, score]);
 
   return (
-    <div className="container mx-auto p-4 flex">
+    <div className="container mx-auto p-4 flex sm:flex-row">
+    <div className="flex-grow">
       {showScore ? (
-        <div className="text-2xl font-bold">
+        <div className="text-2xl font-bold mb-auto">
           <p>Your Score: {score}</p>
           <button
             onClick={() => {
@@ -118,8 +118,9 @@ const Quiz = () => {
           </div>
         )
       )}
-      <Results data={data} />
     </div>
+    <Results data={data} />
+  </div>
   );
 };
 
