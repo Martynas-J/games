7b189859112c6254e5597a9bffe7646913a3adde
Martynas-J <Aurimhas@gmail.com>
2024-01-07 16:21:33 +0200

    fix header small

--- src/app/games/spin/components/header/header.jsx
+++ src/app/games/spin/components/header/header.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const HeaderSpin = () => {
   return (
-    <div className="flex justify-around  bg-teal-600 rounded-t-lg myShadow w-[360px] mx-auto">
+    <div className="flex justify-around  bg-teal-600  sm:rounded-t-lg myShadow w-[360px] mx-auto">
       <div className="p-1 hover:scale-110  cursor-pointer hover:text-gray-300 transition-all duration-500">
         <Link href="/games/spin/info" >Info</Link>
       </div>
