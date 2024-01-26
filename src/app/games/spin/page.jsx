4d9d9a69edce1a683add4f1714c979a77d6d2aca
Martynas-J <aurimhas@gmail.com>
2024-01-26 13:10:54 +0200

    add Speed upgrade v1.5

--- src/app/games/spin/page.jsx
+++ src/app/games/spin/page.jsx
@@ -109,8 +109,8 @@ const Engine = () => {
   const [addMoney, setAddMoney] = useState(false);
   const [buttonClicked, setButtonClicked] = useState(false);
 //1500 1100
-  const spinsTime = 1500 - (1500 - upgradeSpeed * 100);
-  const spinsAnimationTime = 1100 - (1100 - upgradeSpeed * 100);
+  const spinsTime = 1500 - (1500 * (upgradeSpeed / 100));
+  const spinsAnimationTime = 1100 - (1100 * (upgradeSpeed / 100));
 
 
   const handleToggle = () => {
